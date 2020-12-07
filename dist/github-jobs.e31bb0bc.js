e63c0c323fcc6422cebfb74495e784e302b5b1ab
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 09:22:35 +0300

    fetch data from api

--- dist/github-jobs.e31bb0bc.js
+++ dist/github-jobs.e31bb0bc.js
@@ -33877,17 +33877,37 @@ Object.defineProperty(exports, "__esModule", {
 exports.ContextProvider = ContextProvider;
 exports.Context = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
 
-var Context = _react.default.createContext();
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+const Context = _react.default.createContext();
 
 exports.Context = Context;
+const JOBS_API = " https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description";
 
-function ContextProvider(_ref) {
-  var children = _ref.children;
-  return /*#__PURE__*/_react.default.createElement(Context.Provider, null, children);
+function ContextProvider({
+  children
+}) {
+  const [jobs, setJobs] = (0, _react.useState)([]);
+  console.log(jobs);
+
+  async function fetchData() {
+    const response = await fetch(JOBS_API);
+    const data = await response.json();
+    setJobs(data);
+  }
+
+  (0, _react.useEffect)(() => {
+    fetchData();
+  }, []);
+  return /*#__PURE__*/_react.default.createElement(Context.Provider, {
+    value: {
+      jobs
+    }
+  }, children);
 }
 },{"react":"node_modules/react/index.js"}],"index.js":[function(require,module,exports) {
 "use strict";
@@ -33933,7 +33953,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50438" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53258" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- pages/Context.js
+++ pages/Context.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 const Context = React.createContext()
+const JOBS_API = " https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description"
 function ContextProvider({children}) {
+    const [jobs, setJobs] = useState([])
+    console.log(jobs);
+
+    async function fetchData() {
+        const response = await fetch(JOBS_API)
+        const data = await response.json()
+        setJobs(data)
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
-        <Context.Provider>
+        <Context.Provider
+        value={{jobs}}
+        >
             {children}
         </Context.Provider>
     )
