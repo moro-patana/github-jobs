a96e3c91bd45247c10c59f6dddc03fdf436ee153
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 08:01:00 +0300

    wrap the App inside a Router

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 import App from "./pages/App"
-ReactDOM.render(<App/>, document.getElementById('root'))
+import { ContextProvider } from "./pages/Context"
+
+ReactDOM.render(
+    <ContextProvider>
+        <Router>
+            <App/>
+        </Router>
+    </ContextProvider>, document.getElementById('root'))
