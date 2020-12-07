3cb5a7eef57215123406ff9f53da21246794c336
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 10:59:53 +0300

    created a file to store all the list of jobs

---
+++ components/jobsList.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { Context } from "../pages/Context"
+
+export default function jobsList() {
+    const {jobs} = useContext(Context)
+    return (
+        <div>
+            {jobs.map(job => (
+                <article key={job.id}>
+                    <img src={job.company_logo} alt={job.company}/>
+                    <div>
+                        <h3>{job.company}</h3>
+                        <p>{job.title}</p>
+                        <button>{job.type}</button>
+                    </div>
+                    <div>
+                        <span>{job.location}</span>
+                        <span>{job.created_at}</span>
+                    </div>
+                </article>
+            ))}
+        </div>
+    )
+}
