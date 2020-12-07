d03d051045d0efa47b8b63204ac2c1e4c8264e24
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 15:03:02 +0300

    filtered the jobs by title

--- components/SearchJobs.js
+++ components/SearchJobs.js
@@ -1,12 +1,23 @@
-import React from 'react'
-
+import React, { useContext, useState } from 'react'
+import { Context } from "../pages/Context"
 export default function SearchJobs() {
+    const {jobs, setJobs} = useContext(Context)
+    const [searchForJob, setSearchForJob] = useState("")
+
+    function searchForJobInput(e) {
+        e.preventDefault()
+        const filteredJobByName = jobs.filter(job => job.title.toLowerCase().includes(searchForJob))
+        console.log(filteredJobByName)
+        setJobs(filteredJobByName)
+    }
     return (
         <div className="search-jobs">
-            <form className="form-input">
+            <form className="form-input" onSubmit={searchForJobInput}>
                 <input
                 type="text"
                 placeholder="Title, companies, expertise or benefits"
+                value={searchForJob}
+                onChange={(e) => setSearchForJob(e.target.value)}
                 />
                 <button className="search-button">Search</button>
             </form>
