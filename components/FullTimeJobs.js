4513d504f1a4be77add30d1b128fdb6819c1b4f3
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 10:28:36 +0300

    created file for the full time job

---
+++ components/FullTimeJobs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FullTimeJobs() {
+    return (
+        <div>
+             <fieldset>
+               <input type="checkbox"/>
+               <label>Full time</label>
+            </fieldset>
+        </div>
+    )
+}
