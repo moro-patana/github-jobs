c7ac602f25e3f8b5a9dc263f12dd15ff0d3c28bf
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 10:43:52 +0300

    imported FullTimeJobs function

--- pages/App.js
+++ pages/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from "../components/Header"
 import SearchJobs from "../components/SearchJobs"
 import SearchByLocation from "../components/SearchByLocation"
+import FullTimeJobs from "../components/FullTimeJobs"
 
 export default function App() {
     return (
         <div>
             <Header/>
             <SearchJobs/>
+            <FullTimeJobs/>
             <SearchByLocation/>
         </div>
     )
