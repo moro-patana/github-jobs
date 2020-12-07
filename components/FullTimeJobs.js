63310fd6554b2fd8c49ac0293bb640a8c852d566
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-12-07 10:32:10 +0300

    filter jobs by the type

--- components/FullTimeJobs.js
+++ components/FullTimeJobs.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from "../pages/Context"
 
 export default function FullTimeJobs() {
+    const {jobs} = useContext(Context)
+    const fullTimeJobs = jobs.filter(job => job.type === "Full Time")
+    console.log(fullTimeJobs)
     return (
         <div>
              <fieldset>
